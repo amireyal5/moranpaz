ce6117196776f25097eeab0983e3a9d4a965d9c7
עמיר אייל <amirher@gmail.com>
2026-04-03 17:35:01 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAs-example-key", // Note: Replace this with your actual API key from Firebase Console for production
+  apiKey: "AIzaSyDuN9MotAPhb2Efy6EYvITzQaCNjnZCPcM",
   authDomain: "ateam-3cf3d.firebaseapp.com",
   projectId: "ateam-3cf3d",
   storageBucket: "ateam-3cf3d.firebasestorage.app",
   messagingSenderId: "760383317284",
-  appId: "1:760383317284:web:example-id"
+  appId: "1:760383317284:web:6e74239e15a4a3c67f3fb1"
 };
